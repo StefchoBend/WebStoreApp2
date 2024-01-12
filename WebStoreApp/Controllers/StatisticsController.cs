0cd80c3e2b8c13ff1d00d2c087e53a300ae5a7e0
StefchoBend <s.martinov2019@pmg-pernik.com>
2024-01-12 08:10:37 +0200

    etap 8 e gotov

---
+++ WebStoreApp/Controllers/StatisticsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebShopApp.Core.Contracts;
+using WebStoreApp.Models.Statistic;
+
+namespace WebStoreApp.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly IStatisticsService statisticsService;
+
+        public StatisticsController(IStatisticsService statisticsService)
+        {
+            this.statisticsService = statisticsService;
+        }
+
+        public IActionResult Index()
+        {
+            StatisticVM statistics = new StatisticVM();
+
+            statistics.CountClients = statisticsService.CountClients();
+            statistics.CountProducts = statisticsService.CountProducts();
+            statistics.CountOrders = statisticsService.CountOrders();
+            statistics.SumOrders = statisticsService.SumOrders();
+            return View(statistics);
+        }
+    }
+}
--- WebStoreApp/Program.cs
+++ WebStoreApp/Program.cs
@@ -39,6 +39,7 @@ namespace WebStoreApp
             builder.Services.AddTransient<IBrandService, BrandService>();
             builder.Services.AddTransient<IProductService, ProductService>();
             builder.Services.AddTransient<IOrderService, OrderService>();
+            builder.Services.AddTransient<IStatisticsService, StatisticsService>();
 
             var app = builder.Build();
             app.PrepareDatabase();
